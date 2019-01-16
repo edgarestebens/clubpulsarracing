2833924b5fcb91dc09d70833baa85735991872fe
edgarestebens <colachoedgar@hotmail.com>
2019-01-16 10:16:53 -0500

    Restringe las páginas privadas

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+	before_action :private_access, only: [:destroy]
+	before_action :public_access, except: [:destroy]
+	
   def new
   end
 
@@ -16,5 +19,5 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_path
   end
-  
+
 end
